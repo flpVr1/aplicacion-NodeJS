5afe7ff33eded8035b6a861a0bb1f1e114fdb2e2
flpVr1 <112991608+flpVr1@users.noreply.github.com>
2022-09-17 10:45:14 -0300

    Add files via upload

--- source/routes/index.js
+++ source/routes/index.js
@@ -1,1 +1,17 @@
+import { Router } from "express";
 
+const router = Router();
+
+router.get("/", (req, res) => {
+  res.render("index", { title: "Primera web con NodeJS" });
+});
+
+router.get("/about", (req, res) => {
+  res.render("about", { title: "Acerca de la primera página con NodeJS" });
+});
+
+router.get("/contact", (req, res) => {
+  res.render("contacto", { title: "Página de contacto" });
+});
+
+export default router;
